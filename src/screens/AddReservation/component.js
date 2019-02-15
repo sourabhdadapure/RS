6946441c1c790540f3968781ccdd016e4adb4d17
sourabhdadapure <sourabhdadapure@gmail.com>
2019-02-15 01:09:26 -0500

    Adds createReservation mutation

--- src/screens/AddReservation/component.js
+++ src/screens/AddReservation/component.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { DatePickerIOS } from 'react-native'
 import styled from 'styled-components'
-import { Input } from 'react-native-elements'
+import { Input, Button } from 'react-native-elements'
 import DatePicker from 'react-native-datepicker'
+import { Mutation } from 'react-apollo'
+import gql from 'graphql-tag'
 
 const FormWrapper = styled.View`
   flex: 1;
@@ -26,75 +28,130 @@ const DatesWrapper = styled.View`
   align-items: center;
 `
 
+const createReservation = gql`
+  mutation createReservation(
+    $name: String!
+    $hotelName: String!
+    $arrivalDate: String!
+    $departureDate: String!
+  ) {
+    createReservation(
+      data: {
+        name: $name
+        hotelName: $hotelName
+        arrivalDate: $arrivalDate
+        departureDate: $departureDate
+      }
+    ) {
+      name
+      hotelName
+      arrivalDate
+      departureDate
+    }
+  }
+`
+
 export default class AddReservationComponent extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { date: '2016-05-15' }
+    this.state = {
+      name: '',
+      arrivalDate: '2016-05-15',
+      departureDate: '2016-05-15',
+      hotelName: ''
+    }
   }
 
   render() {
+    const { name, arrivalDate, departureDate, hotelName } = this.state
     return (
-      <FormWrapper>
-        <HeaderText> Add A Reservation</HeaderText>
-        <Input inputStyle={{ color: 'white' }} label="Hotel Name" />
-        <DatesWrapper>
-          <DatesText>Arrival:</DatesText>
-          <DatePicker
-            style={{ width: 150, backgroundColor: 'white' }}
-            date={this.state.date}
-            mode="date"
-            placeholder="select date"
-            format="YYYY-MM-DD"
-            minDate="2016-05-01"
-            maxDate="2016-06-01"
-            confirmBtnText="Confirm"
-            cancelBtnText="Cancel"
-            customStyles={{
-              dateIcon: {
-                position: 'absolute',
-                left: 0,
-                top: 4,
-                marginLeft: 0
-              },
-              dateInput: {
-                marginLeft: 36
+      <Mutation mutation={createReservation}>
+        {(createReservation, { data }) => (
+          <FormWrapper>
+            <HeaderText> Add A Reservation</HeaderText>
+            <Input
+              autoCorrect={false}
+              onChangeText={text => this.setState({ name: text })}
+              inputStyle={{ color: 'white' }}
+              label="Name"
+            />
+            <Input
+              onChangeText={text => this.setState({ hotelName: text })}
+              inputStyle={{ color: 'white' }}
+              label="Hotel Name"
+            />
+            <DatesWrapper>
+              <DatesText>Arrival:</DatesText>
+              <DatePicker
+                style={{ width: 150, backgroundColor: 'white' }}
+                date={arrivalDate}
+                mode="date"
+                placeholder="select date"
+                format="YYYY-MM-DD"
+                minDate="2016-05-01"
+                maxDate="2016-06-01"
+                confirmBtnText="Confirm"
+                cancelBtnText="Cancel"
+                customStyles={{
+                  dateIcon: {
+                    position: 'absolute',
+                    left: 0,
+                    top: 4,
+                    marginLeft: 0
+                  },
+                  dateInput: {
+                    marginLeft: 36
+                  }
+                }}
+                onDateChange={date => {
+                  this.setState({ arrival: date })
+                }}
+              />
+            </DatesWrapper>
+            <DatesWrapper>
+              <DatesText>Departure:</DatesText>
+              <DatePicker
+                style={{ width: 150, backgroundColor: 'white' }}
+                date={departureDate}
+                mode="date"
+                placeholder="select date"
+                format="YYYY-MM-DD"
+                minDate="2016-05-01"
+                maxDate="2016-06-01"
+                confirmBtnText="Confirm"
+                cancelBtnText="Cancel"
+                customStyles={{
+                  dateIcon: {
+                    position: 'absolute',
+                    left: 0,
+                    top: 4,
+                    marginLeft: 0
+                  },
+                  dateInput: {
+                    marginLeft: 36
+                  }
+                }}
+                onDateChange={date => {
+                  this.setState({ departure: date })
+                }}
+              />
+            </DatesWrapper>
+            <Button
+              onPress={() =>
+                createReservation({
+                  variables: {
+                    name,
+                    hotelName,
+                    arrivalDate,
+                    departureDate
+                  }
+                })
               }
-            }}
-            onDateChange={date => {
-              this.setState({ date: date })
-            }}
-          />
-        </DatesWrapper>
-        <DatesWrapper>
-          <DatesText>Departure:</DatesText>
-          <DatePicker
-            style={{ width: 150, backgroundColor: 'white' }}
-            date={this.state.date}
-            mode="date"
-            placeholder="select date"
-            format="YYYY-MM-DD"
-            minDate="2016-05-01"
-            maxDate="2016-06-01"
-            confirmBtnText="Confirm"
-            cancelBtnText="Cancel"
-            customStyles={{
-              dateIcon: {
-                position: 'absolute',
-                left: 0,
-                top: 4,
-                marginLeft: 0
-              },
-              dateInput: {
-                marginLeft: 36
-              }
-              // ... You can check the source to find the other keys.
-            }}
-            onDateChange={date => {
-              this.setState({ date: date })
-            }}
-          />
-        </DatesWrapper>
-      </FormWrapper>
+              title="Confirm"
+            />
+          </FormWrapper>
+        )}
+      </Mutation>
     )
   }
 }
